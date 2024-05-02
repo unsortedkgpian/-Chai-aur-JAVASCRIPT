d207dc398665c97835bfc0314547f946112610cf
Aditya Kumar <theadityakumar2810@gmail.com>
2024-05-02 11:43:42 +0530

    dataypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,9 +3,20 @@ let accountEmail = "theadityakumar2810@gmail.com";
 var accountPassword = "12345";
 
 accountCity = "Jaipur";
+let accountState;
 
 // accountId = 2 // Not allowed
 
 // console.log(accountId);
 
-accountEmail="adit@gmail.com"
+accountEmail="adit@gmail.com";
+
+console.log(accountEmail); 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accou]);
+
+/**
+ * 
+ * 
+ * 
+ */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3)// we are using nodejs, not browser
+
+// console.log(3+3) console.log("Hitesh") // error
+
+console.log(3+3);
+
+console.log("Aditya");
+
+
+let name= "Aditya"// string
+let age = 23 // number
+let isLoggedIn = false
+
+// number =>2 to 53
+// bigInt
+// string =>""
+// boolean => true/fals
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "aditya"); 
+
+console.log(typeof null);// object
+
+console.log(typeof undefined);
