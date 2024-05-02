969485dc6a1e47f622effcd830d8bcb3eb3aa684
Aditya Kumar <theadityakumar2810@gmail.com>
2024-05-03 00:18:44 +0530

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let sot = "33";
+
+console.log(typeof sot);
+console.log(typeof (sot));
+
+
+let valueInNumber = Number(sot);
+console.log(typeof valueInNumber);
+console.log(typeof (valueInNumber));
+
+let shot= "239j";
+let shot1 = Number(shot);
+console.log(typeof shot1);
+console.log(shot1);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 
+
+let isLoggedIn= 1
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(isLoggedIn)
