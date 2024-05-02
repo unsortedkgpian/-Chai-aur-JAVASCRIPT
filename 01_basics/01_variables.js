af73429d9a789e12d964bb7a5e54faacb2ddda54
Aditya Kumar <theadityakumar2810@gmail.com>
2024-05-02 09:36:27 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144453;
+let accountEmail = "theadityakumar2810@gmail.com";
+var accountPassword = "12345";
+
+accountCity = "Jaipur";
+
+// accountId = 2 // Not allowed
+
+// console.log(accountId);
+
+accountEmail="adit@gmail.com"
